3d3e01a46cce36d40d8175ddc19f500cb68445cc
khalyomede <khalyomede@gmail.com>
2021-05-31 22:00:32 +0200

    #1 add header function

---
+++ src/test/header_test.v
@@ -0,0 +1,15 @@
+module test
+
+import el
+
+fn test_header_returns_correct_html() {
+	assert el.header({ "": "" }, []) == "<header></header>"
+}
+
+fn test_header_returns_correct_html_with_children() {
+	assert el.header({ "": "" }, ["foo"]) == "<header>foo</header>"
+}
+
+fn test_header_returns_correct_html_with_attributes() {
+	assert el.header({ "data-ssr": "true" }, []) == '<header data-ssr="true"></header>'
+}
